888b9920c5d878ae4616d2d29f4a6564a0d1f0b7
qqbeach <danildzekelev@gmail.com>
2023-12-16 14:20:59 +0200

    Write binary search algoritm.

---
+++ lists/search_algoritms/2_binary_search.py
@@ -0,0 +1,24 @@
+# Works only with sorted list
+#
+
+lst = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 600]
+
+def binary_search(lst, item):
+    start = 0
+    end = len(lst)
+    found = False
+
+    while found == False and start <= end:
+        middle = round((start + end) /2)
+        if lst[middle] == item:
+            return middle
+        if lst[middle] > item:
+            end = middle - 1
+        else:
+            start = middle + 1
+
+    return -1
+
+
+
+binary_search(lst, 600)
